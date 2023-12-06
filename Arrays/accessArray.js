48d7f7d467e95ad4368c2d340c2b14ae2b5417a5
Anshalali <anshalali73@gmail.com>
2023-12-06 16:58:54 +0500

    enh: learn how we find a first variable in array

---
+++ Arrays/accessArray.js
@@ -0,0 +1,4 @@
+// find a first variable in array, (access array data in index)
+var myArray = [20, 30, 40];
+var myData = myArray[0];
+console.log(myData)
--- Arrays/nestArray.js
+++ Arrays/nestArray.js
@@ -1,4 +1,4 @@
-// nest one array within another array
+// nest array is one array within another array
 
 var myArray = [["ahmed", 25], ["ishaque", 24], ["salman", 23], ["anshal", 24]];
 console.log(myArray)
