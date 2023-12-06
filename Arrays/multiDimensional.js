d6b0f6fd8aa5d1efb460fb08e431503f135e015e
Anshalali <anshalali73@gmail.com>
2023-12-06 17:30:18 +0500

    enh: learn how we use multi-dimensional arrays

---
+++ Arrays/multiDimensional.js
@@ -0,0 +1,9 @@
+// find a variable in multiple variables and find any number or caracter in array (access multi-dimensional arrays with index)
+
+var myArray = [
+    [1, 2, 3],
+    [10, 20, 30],
+    [100, 200, 300]
+];
+var myData = myArray[2][1];
+console.log(myData);
