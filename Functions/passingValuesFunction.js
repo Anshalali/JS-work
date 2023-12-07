f8ba126fd7b889a5e9d21e1754a1359b6c48a4fb
Anshalali <anshalali73@gmail.com>
2023-12-07 16:33:40 +0500

    enh: passing values to function with argument

---
+++ Functions/passingValuesFunction.js
@@ -0,0 +1,8 @@
+// passing values function it can be used to pass values to a function
+// (passing values to functions with arguments)
+
+function functionWithArgs(a, b) {
+    console.log(a+b);
+}
+
+functionWithArgs(10, 20)
