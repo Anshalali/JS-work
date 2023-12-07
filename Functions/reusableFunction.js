e4e29982a48d5532bccff7e32615b7d605a9cf90
Anshalali <anshalali73@gmail.com>
2023-12-07 14:01:46 +0500

    enh: how we can use reusableFunction in javascript

---
+++ Functions/reusableFunction.js
@@ -0,0 +1,9 @@
+// reusable function it can be used multiple times
+// (write reusable javascript with functions)
+
+function reusableFunction() {
+    console.log("Hello World");
+    console.log("Goodbye World");
+}
+
+reusableFunction();
