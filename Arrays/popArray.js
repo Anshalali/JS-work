c40d61dc57467ef2d72c70cd1f872563f34cfbab
Anshalali <anshalali73@gmail.com>
2023-12-07 11:40:49 +0500

    how can we remove last element of array by pop()

---
+++ Arrays/popArray.js
@@ -0,0 +1,10 @@
+// pop array it can be used to remove an element from the end of an array
+// (manipulate arrays with pop)
+
+var myArray =[["balls", 20], ["bats", 3], ["helmet", 1]];
+myArray.pop(["helmet", 1]);
+console.log(myArray)
+
+var myArray =[["bikes", 20], ["cars", 3], ["bus", 1]];
+myArray.pop();
+console.log(myArray)
