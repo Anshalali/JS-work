e7c2c59309bd35cc6a831653c87d1b30bdf8d7f7
Anshalali <anshalali73@gmail.com>
2023-12-07 12:32:36 +0500

    enh: learned how to use shift function on array

---
+++ Arrays/shiftArray.js
@@ -0,0 +1,10 @@
+// shift array it can be used to remove an element from the beginning of an array
+// (manipulate arrays with shift)
+
+var myArray = [["dogs", 5], ["ctas", 3], ["hat", 1]];
+var removeFromMyArray = myArray.shift();
+console.log(removeFromMyArray)
+
+var myArray = [["dogs", 5], ["ctas", 3], ["hat", 1]];
+myArray.shift();
+console.log(myArray)
