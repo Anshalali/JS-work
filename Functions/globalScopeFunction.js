a47317c5e0e5b088e158e43445e22c9ae3d3b30c
Anshalali <anshalali73@gmail.com>
2023-12-10 21:52:00 +0500

    enh: i learne golabl scope a function

--- Functions/globalScopeFunction.js
+++ Functions/globalScopeFunction.js
@@ -1,6 +1,3 @@
 // global scope function it can be used to execute a function in global scope and we can use it whole environment of our project
 // (global scope function)
 
-function executeInGlobalScope(func) {
-    func.call(this);
-  }
