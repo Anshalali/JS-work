0e21e929405cb58d48f76cfaafa5222c6040d235
Anshalali <anshalali73@gmail.com>
2023-12-11 13:16:00 +0500

    enh: learned how retrun statement work in js

---
+++ Functions/retrunStatementFunction.js
@@ -0,0 +1,8 @@
+// retrun statement in function it can be used to return a value from a function
+// retrun a value from a function with return statement
+
+function timesix(num) {
+    return num * 6;
+}
+
+console.log(timesix(6));
