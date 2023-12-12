65ead850ae72c39925d250414d138499a62fec89
Anshalali <anshalali73@gmail.com>
2023-12-12 17:33:48 +0500

    enh: how can we use comparison inequality operator in js

---
+++ Functions/comparisonInequalityOperator.js
@@ -0,0 +1,11 @@
+// comparison inequality opreator it is used to compare the values when they are not equal result will be true
+// (comparison inequality operator)
+
+function testNotEqual(val) {
+    if (val != 20) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+console.log(testNotEqual(10));
