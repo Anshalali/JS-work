5bb07b65f0ad1e0a961a9f4c7f33aca9305597c5
Anshalali <anshalali73@gmail.com>
2023-12-12 16:17:17 +0500

    enh: learned comparison with the equality operator in js

---
+++ Functions/comparisonEqualityOperator.js
@@ -0,0 +1,11 @@
+// comparison equality operator it can be used to compare values
+// (comparison equality operator)
+
+function testEqual(val) {
+    if (val == "anshal") {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testEqual("hello"))
