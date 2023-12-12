bbaf3cbe80f856da2e175ba5652665ec79360b52
Anshalali <anshalali73@gmail.com>
2023-12-12 16:59:14 +0500

    enh: learned what is a comparison strict equaltiy operator

---
+++ Functions/comparisonStrictEqualityOperator.js
@@ -0,0 +1,11 @@
+// comparison strict equality operator it can be used to check if two values and types must be equal if not it will be false
+// (comparison operators)
+
+function testStrict(val) {
+    if (val === 7) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testStrict(7));
