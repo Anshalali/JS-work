4fb5ca1875ba46baee1988106ed2f41608a18de6
Anshalali <anshalali73@gmail.com>
2023-12-12 12:59:55 +0500

    enh: learned what is a conitional logic with if statement in js

---
+++ Functions/conditionalLogicIfStatement.js
@@ -0,0 +1,11 @@
+// conditional logic if statement it can be used to make decisions
+// use conditional logic with if statement
+
+function trueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "yes, it's true";
+    }
+    return "no, it's false";
+}
+
+console.log(trueOrFalse(true))
