f806fb675d3f5a4135ce686fee9fd7c7b5a43646
Anshalali <anshalali73@gmail.com>
2023-12-13 13:04:27 +0500

    enh: learned comparison greater then equal to operator in js

---
+++ Functions/comparisonGreaterThenEqualOperator.js
@@ -0,0 +1,15 @@
+// comparison greater then or equal to operator it can be used to check if a value is greater or equal to another value returns true if the left operand is greater than or equal to the right operand, and false otherwise.
+// comparison greater then or equal to operator
+
+function testGreaterThenOrEqual(val) {
+    if (val >= 40) {
+        return "40 or over 40";
+    }
+
+    if (val >= 20) {
+        return "20 or over 20";
+    }
+    return "less than 20";
+}
+
+console.log(testGreaterThenOrEqual("15"));
