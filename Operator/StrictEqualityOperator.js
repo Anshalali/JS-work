d5247e89e37ca9d383f5f3ea3b73c28694caae5c
Anshalali <anshalali73@gmail.com>
2023-12-13 16:37:48 +0500

    enh: learned Comparison strict equality operator in js

--- Operator/StrictEqualityOperator.js
+++ Operator/StrictEqualityOperator.js
@@ -1,4 +1,4 @@
-// comparison strict equality operator it can be used to check if two values and types must be equal if not it will be false
+// Comparison strict equality operator it can be used to check if two values and types must be equal if not it will be false
 // (comparison operators)
 
 function testStrict(val) {
