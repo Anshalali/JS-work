0d84f1a03f2867d564d0ad245163adecbe644488
Anshalali <anshalali73@gmail.com>
2023-12-13 16:23:47 +0500

    enh: comparison with the less than or equal to operator in js

---
+++ Operator/comparisonLessThanEqualOperator.js
@@ -0,0 +1,15 @@
+// comparison less than equal operator it can be used to check if a value is less than or equal to another value  returns true if the left operand is less than or equal to the right operand, and false otherwise
+// comparison less than equal operator
+
+function testLessThenOrEqual(val) {
+    if (val <= 10) {
+        return "smaller then or equal 10"
+    }
+
+    if (val <= 20) {
+        return "smaller then or equal 20"
+    }
+    return "bigger then 20"
+}
+
+console.log(testLessThenOrEqual(28))
