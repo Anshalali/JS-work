24525cf75fd439e19eed0dc1642ec1df9a758946
Anshalali <anshalali73@gmail.com>
2023-12-14 12:05:39 +0500

    enh: introducing else if statement in js

---
+++ If else statements/ElseIfStatement.js
@@ -0,0 +1,14 @@
+// introducing else if statement it can be used to check multiple conditions if the one value is false then it can be used to check another value if 2nd value is false then it can be used to check 3rd value
+// (introducing else if statement)
+
+function testElseIf(val) {
+    if (val >20) {
+        return "Bigger than 20";
+    }else if (val < 10) {
+        return "less than 10";
+    }else {
+        return "between 10 and 20";
+    }
+}
+
+console.log(testElseIf(2))
