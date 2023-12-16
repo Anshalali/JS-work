3419f65c53eabd30c7de31339d862115fa4afd34
Anshalali <anshalali73@gmail.com>
2023-12-16 17:35:47 +0500

    enh: retruning boolean values from functions in js

---
+++ Functions/retruningBooleanValuesFunction.js
@@ -0,0 +1,8 @@
+// retruning boolean values from functions it can be used to return a true or false value from a function
+// (retruning boolean values from functions)
+
+function isLess(a, b) {
+    return a < b;
+}
+  
+console.log(isLess(50, 40));
