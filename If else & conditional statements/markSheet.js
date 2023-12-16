a274d5825d9a529ae3f71f2e856b772baaa63a18
Anshalali <anshalali73@gmail.com>
2023-12-16 11:06:07 +0500

    enh: if else and conditional statements

---
+++ If else & conditional statements/markSheet.js
@@ -0,0 +1,25 @@
+var grade = ["A+", "A", "B", "C", "D", "F"];
+
+function checkyourgrade(marks) {
+
+    if (marks >= 90) {
+        return grade[0];
+    }
+    else if (marks >= 75) {
+        return grade[1];
+    }
+    else if (marks >= 60) {
+        return grade[2];
+    }
+    else if (marks >= 50) {
+        return grade[3];
+    }
+    else if (marks >= 40) {
+        return grade[4];
+    }
+    else {
+        return grade[5];
+    }
+}
+
+console.log((checkyourgrade(39)))
