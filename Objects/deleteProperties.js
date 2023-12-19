da490caec79c500a524452d1298f0e0b71834724
Anshalali <anshalali73@gmail.com>
2023-12-19 13:10:44 +0500

    enh: delete properties an object in js

---
+++ Objects/deleteProperties.js
@@ -0,0 +1,13 @@
+// delete properties it can be used to delete properties from an object
+
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+delete myDog.bark;
+
+console.log(myDog);
