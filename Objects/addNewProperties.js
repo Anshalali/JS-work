ad3ceb1467301b298d9fe1dbd436e02d6f0802c7
Anshalali <anshalali73@gmail.com>
2023-12-19 13:03:37 +0500

    enh: adding new properties in object in js

---
+++ Objects/addNewProperties.js
@@ -0,0 +1,12 @@
+// adding new properties it can be used to add new properties to an object
+// (adding new properties in object)
+
+var myDog = {
+    "name": "coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": []
+};
+
+myDog['bark'] = 'woooof!';
+console.log(myDog);
