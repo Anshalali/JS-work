24b2d46455f68f22ca95bab8064266fb3be58a44
Anshalali <anshalali73@gmail.com>
2023-12-19 12:47:57 +0500

    enh: update object properties in js

---
+++ Objects/updatingObjectProperties.js
@@ -0,0 +1,13 @@
+// updating object properties it can be used to update object properties
+// (update object properties)
+
+var mycat = {
+    "name": "cody",
+    "legs": 4,
+    "tails": 1,
+    "enjoyable": true,
+};
+
+mycat.name = "happy Cody";
+
+console.log(mycat);
