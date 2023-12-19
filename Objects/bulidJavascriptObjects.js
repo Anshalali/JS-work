187b4d1b807360431d89d002084c03404136e14f
Anshalali <anshalali73@gmail.com>
2023-12-19 11:40:42 +0500

    enh: bulid objects in javascript

---
+++ Objects/bulidJavascriptObjects.js
@@ -0,0 +1,11 @@
+// create an object with properties and values it can used to store data
+
+var mycat = {
+    "name": "fluffy",
+    "legs": 4,
+    "tails": 1,
+    "enjoyable": true,
+    "friends": []
+};
+
+console.log(mycat);
