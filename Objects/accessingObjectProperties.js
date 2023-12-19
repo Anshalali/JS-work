63a41ac2497692d7f0fbd410116eee7e438edd4c
Anshalali <anshalali73@gmail.com>
2023-12-19 11:51:21 +0500

    enh: access object properties with dot notations in js

---
+++ Objects/accessingObjectProperties.js
@@ -0,0 +1,16 @@
+// accessing object properties with dot notation it can be used to access object properties
+// (access object properties with dot notation)
+
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "pants": "shorts",
+    "shoes": "sneakers"
+};
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+var shoesValue = testObj.shoes;
+
+console.log(hatValue, shirtValue, shoesValue);
+console.log(testObj);
