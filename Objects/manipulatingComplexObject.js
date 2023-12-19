49dcd897aed9f753152466d9dba45329f091d9d7
Anshalali <anshalali73@gmail.com>
2023-12-19 16:50:11 +0500

    enh: manipulating complex object in js

---
+++ Objects/manipulatingComplexObject.js
@@ -0,0 +1,30 @@
+// manipulating complex object it can be used to manipulate complex objects use for add another object in variable
+
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+
+    {
+        "artist": "Michael Jackson",
+        "title": "Thriller",
+        "release_year": 1982,
+        "formats": [
+            "CD",
+            "LP",
+            "Cassette"
+            
+        ],                              
+
+    },
+];
+
+console.log(myMusic);
