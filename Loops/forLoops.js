13aaa4f09d3eef210a7cb6fe87603476e45946e9
Anshalali <anshalali73@gmail.com>
2023-12-20 17:19:59 +0500

    enh: for loops in javascript

---
+++ Loops/forLoops.js
@@ -0,0 +1,8 @@
+// for loops are used to execute a block of code a specified number of times it repeats until a specified condition evaluates to false
+
+var myArray = [];
+for (var a = 4; a < 10; a = a + 2) {
+    myArray.push(a);
+}
+
+console.log(myArray);
