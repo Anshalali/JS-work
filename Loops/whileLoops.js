38628f56328b0b95678c8b2f91d43f23000001ce
Anshalali <anshalali73@gmail.com>
2023-12-20 16:37:38 +0500

    enh: iterate with javascript while loops

---
+++ Loops/whileLoops.js
@@ -0,0 +1,12 @@
+// while loops are used to execute a block of code as long as a specified condition is true
+// The while statement creates a loop (around a code block) that is executed while a condition is true The loop runs while the condition is true Otherwise it stops
+
+var myArray = [];
+
+var a = 10;
+while (a > 5) {
+    myArray.push(a);
+    a--;
+}
+
+console.log(myArray);
