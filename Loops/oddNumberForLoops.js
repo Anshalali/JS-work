0ddbe54abfb6e62db0f9e4e79c5451474039fe17
Anshalali <anshalali73@gmail.com>
2023-12-21 10:52:08 +0500

    enh: itreate odd and even number with for loops

---
+++ Loops/oddNumberForLoops.js
@@ -0,0 +1,17 @@
+// itrate odd and even numbers with a for loops it can be used to iterate odd numbers or find to odd number in array
+
+var myArray = [];
+
+for (var a = 2; a < 20; a += 2) {
+    myArray.push(a);
+}
+
+console.log(myArray)
+
+var ourArray = [];
+
+for (var a = 1; a < 20; a += 2) {
+    ourArray.push(a);
+}
+
+console.log(ourArray);
