5269f485b7a954329f817304f7d04f08d2acaa0a
Anshalali <anshalali73@gmail.com>
2023-12-23 19:03:53 +0500

    enh: how loops is runing in js

---
+++ Loops/howLoopsRun.js
@@ -0,0 +1,10 @@
+// how the loops is runing in javascript
+
+arr  = [4,5,9,7,15,230,10]
+
+console.log(arr[3]);
+console.log("\n\n\n")
+
+for (var a = 0; a<arr.length; a++){
+    console.log(arr[a]+1)
+}
