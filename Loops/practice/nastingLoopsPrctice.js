435bc9a99e87dbe4e1b09e1b3f5453629a91b21b
Anshalali <anshalali73@gmail.com>
2023-12-24 06:19:15 -0800

    enh: practice of loops

---
+++ Loops/practice/nastingLoopsPrctice.js
@@ -0,0 +1,40 @@
+// var myArray = [];
+
+// for (var a = 0; a < 100; a++) {
+//     myArray.push(a);
+// }
+
+// console.log(myArray);
+
+// for (var a = 0; a <= 100; a++) {
+//     console.log(a);
+// }
+
+
+
+
+// for (var a =1; a <=1000; a++) {
+//     if (a % 3 === 0) {
+//         console.log(a);
+//     }
+// }
+
+
+
+// var  temp  = 5;
+
+// for (var a = 1; a <= 10; a++) { {
+// }
+
+// if (temp * a === 25) {
+//     temp = 2;
+// }
+// console.log(temp * a);
+
+// }
+
+
+for ( var a = 65; a <= 89; a++) {
+    var letter = String.fromCharCode(a);
+}
+console.log(letter);
