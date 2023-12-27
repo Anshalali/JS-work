81543093962f7fcec2a53fd07616fbb1abc13622
Anshalali <anshalali73@gmail.com>
2023-12-27 13:09:58 +0500

    enh: do while loops in js

---
+++ Loops/doWhileLoop.js
@@ -0,0 +1,13 @@
+// do while loop it can be used to execute a block of code as long as a specified condition is true
+
+var myArray = [];
+var a = 20;
+
+    do 
+    {
+        myArray.push(a);
+        a++;
+    } 
+    while (a < 10);
+
+console.log(a,myArray)
